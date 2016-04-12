fd725d73a57cdb8ad60c1fd570e51446594ed6c3
Pei Sun <peis@alluxio.com>
2016-04-12 11:59:20 -0700

    .

--- bp.sh
+++ bp.sh
@@ -1,8 +1,9 @@
-#!/bin/bash                                                                                        
+#!/bin/bash
 
 for ((i = 0; i < $1; i++)); do
    for ((j = 0; j < $2; j++)); do
      time ( echo "i:$i,j:$j" > /mnt/ramdisk/alluxioworker/f_${i}_${j} ) >> /tmp/stats
+   done
 done
 
 
