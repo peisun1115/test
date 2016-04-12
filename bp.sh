5276b283df2193c8e509bd14b1baef36d396a8ec
Pei Sun <peis@alluxio.com>
2016-04-12 15:51:49 -0700

    .

--- bp.sh
+++ bp.sh
@@ -3,18 +3,21 @@
 # $1: i
 # $2: max_j
 # $3: prefix
+# $4: dir
 _helper() {
     for ((j = 0; j < $2; j++)); do
-	   echo "i:$1,j:$j" > /mnt/ramdisk/alluxioworker/f_${3}_${1}_${j}
+	   echo "i:$1,j:$j" > ${4}/f_${3}_${1}_${j}
     done
 }
 
 >/tmp/stats
+DIR="/mnt/ramdisk/alluxioworker"
+export TIMEFORMAT='%3R %3U %3S'
 for ((i = 0; i < $1; i++)); do
-    export TIMEFORMAT='%3R %3U %3S'
     if [[ $4 = "-t" ]]; then
-        sudo mkdir -p /mnt/ramdisk/alluxioworker/$i
+        mkdir -p ${DIR}/$i
+        DIR="${DIR}/$i"
     fi
-    time ( sudo _helper $i $2 $3 ) &>> /tmp/stats 
+    time ( _helper $i $2 $3 ${DIR}) &>> /tmp/stats 
 done
 
