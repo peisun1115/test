88e3f8c7e2529e9ab76b1de7ba4937093cbbf463
Pei Sun <peis@alluxio.com>
2016-04-12 14:25:59 -0700

    .

--- bp.sh
+++ bp.sh
@@ -2,16 +2,16 @@
 
 >/tmp/stats
 for ((i = 0; i < $1; i++)); do
-    real=0
-    sys=0
-    usr=0
-    for ((j = 0; j < $2; j++)); do
-	time -p ( echo "i:$i,j:$j" > /mnt/ramdisk/alluxioworker/f_${i}_${j} ) &>/tmp/s
-	real=$(echo "${real} + $(cat /tmp/s | grep 'real' | cut -d ' ' -f2)" | bc)
-	sys=$(echo "${sys} + $(cat /tmp/s | grep 'sys' | cut -d ' ' -f2)" | bc)
-	usr=$(echo "${usr} + $(cat /tmp/s | grep 'usr' | cut -d ' ' -f2)" | bc)
-   done
-   echo ${real}" "${sys}" "${usr} >> /tmp/stats
+    export TIMEFORMAT='%3R %3U %3S'
+    time ( _helper $i $2 $3 ) &> /tmp/stats 
 done
 
+# $1: i
+# $2: max_j
+# $3: prefix
+_helper() {
+    for ((j = 0; j < $2; j++)); do
+	    echo "i:$1,j:$j" > /mnt/ramdisk/alluxioworker/f_${3}_${1}_${j}
+    done
+}
 
