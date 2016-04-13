c3ff9b68ada8da9a8f01342e68606fecaadb0b77
Pei Sun <peis@alluxio.com>
2016-04-12 22:57:04 -0700

    .

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 # AWS related. Copy this function to setup a new aws instance.
-function setup() {
     alias mm="mvn -Pdeveloper -T 2C clean install -Dmaven.javadoc.skip=true -DskipTests -Dlicense.skip=true -Dcheckstyle.skip=true -Dfindbugs.skip=true"
     sudo yum update
     sudo yum install -y -q java-1.7.0-openjdk-devel.x86_64
@@ -16,5 +15,4 @@ function setup() {
     cd
     git clone https://github.com/amix/vimrc.git ~/.vim_runtime
     sh ~/.vim_runtime/install_awesome_vimrc.sh
-}
 
