bb7e0558d03e8b523c655e4635d0c31d6c1a966a
Pei Sun <peis@alluxio.com>
2016-04-12 23:26:13 -0700

    rename exp5 exp5_0

--- bp_exp5.sh
+++ bp_exp5.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-./bp.sh ALLUXIO "Alluxio:" 2 10 "./exp5_0" 0
-./bp.sh LOCAL "/mnt/ramdisk/alluxioworker" 25 1000 "./exp5_1" 1
-./bp.sh ALLUXIO "Alluxio:" 2 10 "./exp5_2" 2
-./bp.sh LOCAL "/mnt/ramdisk/alluxioworker" 250 1000 "./exp5_3" 3
-./bp.sh ALLUXIO "Alluxio:" 2 10 "./exp5_4" 4
-./bp.sh LOCAL "/mnt/ramdisk/alluxioworker" 250 10000 "./exp5_5" 5
-./bp.sh ALLUXIO "Alluxio:" 2 10 "./exp5_6" 6
+./bp.sh ALLUXIO "Alluxio:" 2 10 "./exp5_0_0" 0
+./bp.sh LOCAL "/mnt/ramdisk/alluxioworker" 25 1000 "./exp5_0_1" 1
+./bp.sh ALLUXIO "Alluxio:" 2 10 "./exp5_0_2" 2
+./bp.sh LOCAL "/mnt/ramdisk/alluxioworker" 250 1000 "./exp5_0_3" 3
+./bp.sh ALLUXIO "Alluxio:" 2 10 "./exp5_0_4" 4
+./bp.sh LOCAL "/mnt/ramdisk/alluxioworker" 250 10000 "./exp5_0_5" 5
+./bp.sh ALLUXIO "Alluxio:" 2 10 "./exp5_0_6" 6
 
