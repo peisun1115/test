356b0588042fa491f0e16cd12e05c7e570bb3d10
Pei Sun <peisun1115@gmail.com>
2016-04-14 00:56:27 +0000

    .

--- setup.sh
+++ setup.sh
@@ -1,12 +1,6 @@
 # AWS related. Copy this function to setup a new aws instance.
     mm="mvn -Pdeveloper -T 2C clean install -Dmaven.javadoc.skip=true -DskipTests -Dlicense.skip=true -Dcheckstyle.skip=true -Dfindbugs.skip=true"
-    sudo yum update
-    sudo yum install -y -q java-1.7.0-openjdk-devel.x86_64
-    sudo wget 'http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo' -O  /etc/yum.repos.d/epel-apache-maven.repo
-    sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
-    sudo yum install -y apache-maven
-    mvn --version
-    sudo yum install git
+    sudo apt-get install maven
     mkdir -p ~/workspace
     cd ~/workspace
     git clone https://github.com/peisun1115/alluxio.git
