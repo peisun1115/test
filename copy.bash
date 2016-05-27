41d9300027f603c71ae9b000984076b463dbe759
Pei Sun <peis@alluxio.com>
2016-05-27 13:46:02 -0700

    update copy script

--- copy.bash
+++ copy.bash
@@ -4,14 +4,17 @@ function copy {
   TL=$2
   WT=$3
 
+  cp ~/workspace/test/${OP}_${WT}_1.0.1_C8_${TL} c8_1.0.1.dat
+  cp ~/workspace/test/${OP}_${WT}_1.1.0_C8_${TL} c8_1.1.0.dat
+
   cp ~/workspace/test/${OP}_${WT}_1.0.1_C32_${TL} c32_1.0.1.dat
   cp ~/workspace/test/${OP}_${WT}_1.1.0_C32_${TL} c32_1.1.0.dat
+
   cp ~/workspace/test/HDFS_${OP}_${WT}_1.1.0_C32_${TL} c32_hdfs.dat
   cp ~/workspace/test/HDFS_${OP}_${WT}_1.1.0_C8_${TL} c8_hdfs.dat
-  cp ~/workspace/test/${OP}_1.0.1_${WT}_RJ_C32_${TL} c32_rj_1.0.1.dat
-  cp ~/workspace/test/${OP}_1.1.0_${WT}_RJ_C32_${TL} c32_rj_1.1.0.dat
-  cp ~/workspace/test/${OP}_1.0.1_${WT}_C8_${TL} c8_1.0.1.dat
-  cp ~/workspace/test/${OP}_1.1.0_${WT}_C8_${TL} c8_1.1.0.dat
+
+  cp ~/workspace/test/RJ_${OP}_${WT}_1.0.1_C32_${TL} c32_rj_1.0.1.dat
+  cp ~/workspace/test/RJ_${OP}_${WT}_1.1.0_C32_${TL} c32_rj_1.1.0.dat
 }
 
 cd ${1}/create_file_CACHE_THROUGH
@@ -20,7 +23,7 @@ make
 cd ../..
 
 cd ${1}/create_file_MUST_CACHE
-copy CREATE_FILE ${1}
+copy CREATE_FILE ${1} MUST_CACHE
 make
 cd ../..
 
@@ -30,7 +33,7 @@ make
 cd ../..
 
 cd ${1}/delete_file_MUST_CACHE
-copy DELETE_FILE ${1}
+copy DELETE_FILE ${1} MUST_CACHE
 make
 cd ../..
 
@@ -40,7 +43,7 @@ make
 cd ../..
 
 cd ${1}/get_status_MUST_CACHE
-copy LIST_STATUS ${1}
+copy LIST_STATUS ${1} MUST_CACHE
 make
 cd ../..
 
@@ -50,7 +53,7 @@ make
 cd ../..
 
 cd ${1}/list_status_MUST_CACHE
-copy LIST_STATUS_Dir ${1}
+copy LIST_STATUS_Dir ${1} MUST_CACHE
 make
 cd ../..
 
