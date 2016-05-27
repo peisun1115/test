13ccff2daa3f193373ac8a379424fecbe15fa127
Pei Sun <peis@alluxio.com>
2016-05-27 13:29:19 -0700

    round 8

--- copy.bash
+++ copy.bash
@@ -2,58 +2,55 @@
 function copy {
   OP=$1
   TL=$2
-  CT=""
-  if [[ ! -z $3 ]]; then
-      CT="CT_"
-  fi
-  
-  cp ~/workspace/test/${OP}_1.0.1_${CT}C32_${TL} c32_1.0.1.dat
-  cp ~/workspace/test/${OP}_1.1.0_${CT}C32_${TL} c32_1.1.0.dat
-  cp ~/workspace/test/${OP}_1.1.0_HDFS_C32_${TL} c32_hdfs.dat
-  cp ~/workspace/test/${OP}_1.0.1_${CT}RJ_C32_${TL} c32_rj_1.0.1.dat
-  cp ~/workspace/test/${OP}_1.1.0_${CT}RJ_C32_${TL} c32_rj_1.1.0.dat
-  cp ~/workspace/test/${OP}_1.0.1_${CT}C8_${TL} c8_1.0.1.dat
-  cp ~/workspace/test/${OP}_1.1.0_${CT}C8_${TL} c8_1.1.0.dat
+  WT=$3
+
+  cp ~/workspace/test/${OP}_${WT}_1.0.1_C32_${TL} c32_1.0.1.dat
+  cp ~/workspace/test/${OP}_${WT}_1.1.0_C32_${TL} c32_1.1.0.dat
+  cp ~/workspace/test/${OP}_${WT}_1.1.0_HDFS_C32_${TL} c32_hdfs.dat
+  cp ~/workspace/test/${OP}_1.0.1_${WT}_RJ_C32_${TL} c32_rj_1.0.1.dat
+  cp ~/workspace/test/${OP}_1.1.0_${WT}_RJ_C32_${TL} c32_rj_1.1.0.dat
+  cp ~/workspace/test/${OP}_1.0.1_${WT}_C8_${TL} c8_1.0.1.dat
+  cp ~/workspace/test/${OP}_1.1.0_${WT}_C8_${TL} c8_1.1.0.dat
   cp ~/workspace/test/${OP}_1.1.0_HDFS_C8_${TL} c8_hdfs.dat
 }
 
 cd ${1}/create_file_CACHE_THROUGH
-copy CreateFile ${1} CT
+copy CREATE_FILE ${1} CACHE_THROUGH
 make
 cd ../..
 
 cd ${1}/create_file_MUST_CACHE
-copy CreateFile ${1}
+copy CREATE_FILE ${1}
 make
 cd ../..
 
 cd ${1}/delete_file_CACHE_THROUGH
-copy DeleteFile ${1} CT
+copy DELETE_FILE ${1} CACHE_THROUGH
 make
 cd ../..
 
 cd ${1}/delete_file_MUST_CACHE
-copy DeleteFile ${1}
+copy DELETE_FILE ${1}
 make
 cd ../..
 
 cd ${1}/get_status_CACHE_THROUGH
-copy ListStatus ${1} CT
+copy LIST_STATUS ${1} CACHE_THROUGH
 make
 cd ../..
 
 cd ${1}/get_status_MUST_CACHE
-copy ListStatus ${1}
+copy LIST_STATUS ${1}
 make
 cd ../..
 
 cd ${1}/list_status_CACHE_THROUGH
-copy ListStatus_Dir ${1} CT
+copy LIST_STATUS_Dir ${1} CACHE_THROUGH
 make
 cd ../..
 
 cd ${1}/list_status_MUST_CACHE
-copy ListStatus_Dir ${1}
+copy LIST_STATUS_Dir ${1}
 make
 cd ../..
 
