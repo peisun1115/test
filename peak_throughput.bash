de3b5c7d4c3465788a37c238f68392104f1fa9a5
Pei Sun <peis@alluxio.com>
2016-05-30 16:47:49 -0700

    peak throughput summary

--- peak_throughput.bash
+++ peak_throughput.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 peak_throughput() {
-    cat $1 | awk 'BEGIN{max = -1} $2 > max {max = $2} END{print $2}'
+    cat $1 | awk 'BEGIN{max = -1} $2 > max {max = $2} END{print max}'
 }
 > peak_thr_summary; for i in $( ls | grep 'throughput$' ); do echo -ne $i" " >> peak_thr_summary; peak_throughput $i >> peak_thr_summary;done;
