317116a6381932fdff1b194f20209998cc58c378
Pei Sun <peis@alluxio.com>
2016-05-30 21:34:03 -0700

    round 9

--- copy.bash
+++ copy.bash
@@ -10,50 +10,50 @@ function copy {
   cp ~/workspace/test/${OP}_${WT}_1.0.1_C32_${TL} c32_1.0.1.dat
   cp ~/workspace/test/${OP}_${WT}_1.1.0_C32_${TL} c32_1.1.0.dat
 
-  cp ~/workspace/test/HDFS_${OP}_${WT}_1.1.0_C32_${TL} c32_hdfs.dat
-  cp ~/workspace/test/HDFS_${OP}_${WT}_1.1.0_C8_${TL} c8_hdfs.dat
+  cp ~/workspace/test/${OP}_hdfs_${WT}_1.1.0_C32_${TL} c32_hdfs.dat
+  cp ~/workspace/test/${OP}_hdfs_${WT}_1.1.0_C8_${TL} c8_hdfs.dat
 
-  cp ~/workspace/test/RJ_${OP}_${WT}_1.0.1_C32_${TL} c32_rj_1.0.1.dat
-  cp ~/workspace/test/RJ_${OP}_${WT}_1.1.0_C32_${TL} c32_rj_1.1.0.dat
+  cp ~/workspace/test/${OP}_rj_${WT}_1.0.1_C32_${TL} c32_rj_1.0.1.dat
+  cp ~/workspace/test/${OP}_rj_${WT}_1.1.0_C32_${TL} c32_rj_1.1.0.dat
 }
 
 cd ${1}/create_file_CACHE_THROUGH
-copy CREATE_FILE ${1} CACHE_THROUGH
+copy CREATE_FILE ${1} CT 
 make
 cd ../..
 
 cd ${1}/create_file_MUST_CACHE
-copy CREATE_FILE ${1} MUST_CACHE
+copy CREATE_FILE ${1} MC 
 make
 cd ../..
 
 cd ${1}/delete_file_CACHE_THROUGH
-copy DELETE ${1} CACHE_THROUGH
+copy DELETE ${1} CT 
 make
 cd ../..
 
 cd ${1}/delete_file_MUST_CACHE
-copy DELETE ${1} MUST_CACHE
+copy DELETE ${1} MC 
 make
 cd ../..
 
 cd ${1}/get_status_CACHE_THROUGH
-copy LIST_STATUS ${1} CACHE_THROUGH
+copy LIST_STATUS ${1} CT 
 make
 cd ../..
 
 cd ${1}/get_status_MUST_CACHE
-copy LIST_STATUS ${1} MUST_CACHE
+copy LIST_STATUS ${1} MC 
 make
 cd ../..
 
 cd ${1}/list_status_CACHE_THROUGH
-copy LIST_STATUS_Dir ${1} CACHE_THROUGH
+copy LIST_STATUS_D ${1} CT 
 make
 cd ../..
 
 cd ${1}/list_status_MUST_CACHE
-copy LIST_STATUS_Dir ${1} MUST_CACHE
+copy LIST_STATUS_D ${1} MC 
 make
 cd ../..
 
--- maxfile/parse_maxfile.bash
+++ maxfile/parse_maxfile.bash
@@ -1,3 +1,4 @@
-#/bin/bash
+#!/usr/bin/env bash
 
-cat "${1}_MaxFile_C80"  | grep '^CREATE_FILE_MUST_CACHE.*_C80' -B2 | sed '/[a-z]/d' | grep '[1-9]' | sed 's/\(.*\) .*\\/\1/' > ${1}_MaxFile_runtime
+cat "${1}_MaxFile"  | grep '^CREATE_FILE_MF_MC.*_C80' -B2 | sed '/[a-z]/d' | grep '[1-9]' | sed 's/\(.*\) .*\\/\1/' > ${1}_MaxFile_runtime
+cat "${1}_MaxFile" | awk 'BEGIN{count = 0} /./ {count = $1} END{print count}' >> ${1}_MaxFile_runtime
