980ade55e3034b8d9cfc0563853030facc6b5277
Pei Sun <peis@alluxio.com>
2016-06-01 18:04:00 -0700

    .

--- maxfile/parse_maxfile.bash
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-cat "${1}_MaxFile"  | grep '^CREATE_FILE_MF_MC.*_C80' -B2 | sed '/[a-z]/d' | grep '[1-9]' | sed 's/\(.*\) .*\\/\1/' > ${1}_MaxFile_runtime
-cat "${1}_MaxFile" | awk 'BEGIN{count = 0} /./ {count = $1} END{print count}' >> ${1}_MaxFile_runtime
