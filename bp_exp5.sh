25575a455988f5ebe48de719e0f4c9b516365af0
Pei Sun <peis@alluxio.com>
2016-06-30 15:56:43 -0700

    remove unused stuff

--- bp_exp5.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-./bp.sh ALLUXIO "Alluxio:" 2 10 "./exp5_0_0" 0
-./bp.sh LOCAL "/mnt/ramdisk/alluxioworker" 25 1000 "./exp5_0_1" 1
-./bp.sh ALLUXIO "Alluxio:" 2 10 "./exp5_0_2" 2
-./bp.sh LOCAL "/mnt/ramdisk/alluxioworker" 250 1000 "./exp5_0_3" 3
-./bp.sh ALLUXIO "Alluxio:" 2 10 "./exp5_0_4" 4
-./bp.sh LOCAL "/mnt/ramdisk/alluxioworker" 250 10000 "./exp5_0_5" 5
-./bp.sh ALLUXIO "Alluxio:" 2 10 "./exp5_0_6" 6
-
